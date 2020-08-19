1a7f9c398266fc4753d98aa4ab890f6b6f46acf1
Phillip A Pearson <59122457+phillpearsondev@users.noreply.github.com>
2020-08-19 20:59:51 +0100

    moved several functions that draw graphics from game_logi.js into a new file called display_utils.js

---
+++ assets/js/display_utils.js
@@ -0,0 +1,101 @@
+// This function sets up and updates our score
+function displayScore() {
+
+    scoreText = new PIXI.Text("Score:");
+    scoreText.anchor.set(0);
+    scoreText.x = app.view.width - 790;
+    scoreText.y = app.view.height - 590;
+    scoreText.visible = false;
+    scoreText.style = new PIXI.TextStyle({
+        fill: 0xffFFFF,
+        fontSize: 14,
+        fontFamily: "Lucida Console",
+        fontStyle: "normal",
+        strokeThickness: 0
+    })
+    scoreText.text = "Score: " + score;
+};
+
+function createBackground(texture) {
+    let tiling = new PIXI.TilingSprite(texture, 800, 600);
+    tiling.position.set(0, 0);
+    app.stage.addChild(tiling);
+
+    return tiling;
+};
+
+// This function draws an asteroid in the centre of the screens width
+function asteroidPhase() {
+    asteroid = PIXI.Sprite.from(app.loader.resources.asteroid.texture);
+    asteroid.anchor.set(0.5);
+    asteroid.x = app.view.width - 600;
+    asteroid.y = app.view.height - 700;
+    asteroid.speed = asteroidSpeed;
+    app.stage.addChild(asteroid);
+
+    return asteroid;
+};
+
+// This function draws an asteroid on the right side of the screens width
+function asteroidPhase1() {
+    asteroid = PIXI.Sprite.from(app.loader.resources.asteroid.texture);
+    asteroid.anchor.set(0.5);
+    asteroid.x = app.view.width - 200;
+    asteroid.y = app.view.height - 700;
+    asteroid.speed = asteroidSpeed;
+    app.stage.addChild(asteroid);
+
+    return asteroid;
+};
+
+// This function draws an asteroid on the left side of the screens width
+function asteroidPhase2() {
+    asteroid = PIXI.Sprite.from(app.loader.resources.asteroid.texture);
+    asteroid.anchor.set(0.5);
+    asteroid.x = app.view.width - 400;
+    asteroid.y = app.view.height - 700;
+    asteroid.speed = asteroidSpeed;
+    app.stage.addChild(asteroid);
+
+    return asteroid;
+};
+
+// This function draws an enemy sprite in the centre of the screens width
+function drawPlayer() {
+    ship = PIXI.Sprite.from(app.loader.resources.ship.texture);
+    ship.anchor.set(0.5);
+    ship.x = app.view.width / 2;
+    ship.y = app.view.height / 1.1;
+
+    return ship;
+};
+
+// This function draws an enemy sprite on the left side of the screens width
+function drawEnemy() {
+    enemy = PIXI.Sprite.from(app.loader.resources["enemy"].texture);
+    enemy.anchor.set(0.5);
+    enemy.x = app.view.width / 3;
+    enemy.y = app.view.height - 800;
+    app.stage.addChild(enemy);
+    return enemy;
+};
+
+// This function draws an enemy sprite on the right side of the screens width
+function drawEnemy2() {
+    enemy = PIXI.Sprite.from(app.loader.resources["enemy"].texture);
+    enemy.anchor.set(0.5);
+    enemy.x = app.view.width / 1.5;
+    enemy.y = app.view.height - 800;
+    enemy.phase = 0;
+    app.stage.addChild(enemy);
+    return enemy;
+}
+
+// This function updates the positions of tiling in the parallaxing background
+// This is what gives motion to the background and makes us feel like we are flying through space
+function updateBackground() {
+    backgroundX = (backgroundX + backgroundSpeed);
+    backgroundFront.tilePosition.y = backgroundX;
+    backgroundMiddle.tilePosition.y = backgroundX / 2;
+    backgroundBack.tilePosition.y = backgroundX / 4;
+};
--- assets/js/game_logic.js
+++ assets/js/game_logic.js
@@ -1,23 +1,6 @@
 // This document contains the bulk of the logic for how various aspects of the game will work
 // which consists of mostly functions, inclusive in the gameLoop function.
 
-// This function sets up and updates our score
-function displayScore() {
-
-    scoreText = new PIXI.Text("Score:");
-    scoreText.anchor.set(0);
-    scoreText.x = app.view.width - 790;
-    scoreText.y = app.view.height - 590;
-    scoreText.visible = false;
-    scoreText.style = new PIXI.TextStyle({
-        fill: 0xffFFFF,
-        fontSize: 14,
-        fontFamily: "Lucida Console",
-        fontStyle: "normal",
-        strokeThickness: 0
-    })
-    scoreText.text = "Score: " + score;
-};
 
 // This function when called removes the title text, adds our Player ship, an Enemy and an Asteroid to the screen
 // as well as calls our function to display the Players score
@@ -34,14 +17,6 @@ function initLevel() {
 
 };
 
-function createBackground(texture) {
-    let tiling = new PIXI.TilingSprite(texture, 800, 600);
-    tiling.position.set(0, 0);
-    app.stage.addChild(tiling);
-
-    return tiling;
-};
-
 // This function checks if two variables "a, b" collide with each other
 function checkPlayerCollision(a, b) {
     let boundBoxA = a.getBounds();
@@ -53,73 +28,6 @@ function checkPlayerCollision(a, b) {
         boundBoxA.y + boundBoxA.height > boundBoxB.y &&
         boundBoxA.y < boundBoxB.y + boundBoxB.height;
 };
-
-// This function draws an asteroid in the centre of the screens width
-function asteroidPhase() {
-    asteroid = PIXI.Sprite.from(app.loader.resources.asteroid.texture);
-    asteroid.anchor.set(0.5);
-    asteroid.x = app.view.width - 600;
-    asteroid.y = app.view.height - 700;
-    asteroid.speed = asteroidSpeed;
-    app.stage.addChild(asteroid);
-
-    return asteroid;
-};
-
-// This function draws an asteroid on the right side of the screens width
-function asteroidPhase1() {
-    asteroid = PIXI.Sprite.from(app.loader.resources.asteroid.texture);
-    asteroid.anchor.set(0.5);
-    asteroid.x = app.view.width - 200;
-    asteroid.y = app.view.height - 700;
-    asteroid.speed = asteroidSpeed;
-    app.stage.addChild(asteroid);
-
-    return asteroid;
-};
-
-// This function draws an asteroid on the left side of the screens width
-function asteroidPhase2() {
-    asteroid = PIXI.Sprite.from(app.loader.resources.asteroid.texture);
-    asteroid.anchor.set(0.5);
-    asteroid.x = app.view.width - 400;
-    asteroid.y = app.view.height - 700;
-    asteroid.speed = asteroidSpeed;
-    app.stage.addChild(asteroid);
-
-    return asteroid;
-};
-
-// This function draws an enemy sprite in the centre of the screens width
-function drawPlayer() {
-    ship = PIXI.Sprite.from(app.loader.resources.ship.texture);
-    ship.anchor.set(0.5);
-    ship.x = app.view.width / 2;
-    ship.y = app.view.height / 1.1;
-
-    return ship;
-};
-
-// This function draws an enemy sprite on the left side of the screens width
-function drawEnemy() {
-    enemy = PIXI.Sprite.from(app.loader.resources["enemy"].texture);
-    enemy.anchor.set(0.5);
-    enemy.x = app.view.width / 3;
-    enemy.y = app.view.height - 800;
-    app.stage.addChild(enemy);
-    return enemy;
-};
-
-// This function draws an enemy sprite on the right side of the screens width
-function drawEnemy2() {
-    enemy = PIXI.Sprite.from(app.loader.resources["enemy"].texture);
-    enemy.anchor.set(0.5);
-    enemy.x = app.view.width / 1.5;
-    enemy.y = app.view.height - 800;
-    enemy.phase = 0;
-    app.stage.addChild(enemy);
-    return enemy;
-}
 
 // This function updates aspects like sprite position in delta time, in connection with gameLoop
 function updateLevel(delta) {
@@ -218,15 +126,6 @@ function keyDown(e) {
 function keyUp(e) {
     //console.log(e.keyCode);
     keys[e.keyCode] = false;
-};
-
-// This function updates the positions of tiling in the parallaxing background
-// This is what gives motion to the background and makes us feel like we are flying through space
-function updateBackground() {
-    backgroundX = (backgroundX + backgroundSpeed);
-    backgroundFront.tilePosition.y = backgroundX;
-    backgroundMiddle.tilePosition.y = backgroundX / 2;
-    backgroundBack.tilePosition.y = backgroundX / 4;
 };
 
 // sends to game over screen //
